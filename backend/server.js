539667e6fd655173be5f32e8f2d52f71bcd44807
Franco Abarca <zoeyabarca1984@gmail.com>
2026-04-13 14:22:11 -0600

    backend listo para railway

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,10 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
 
 app.use('/api/user', require('./routes/protected'));
+
